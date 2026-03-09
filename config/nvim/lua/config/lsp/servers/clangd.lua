b1bc4075590b21484b5e24abfdbc2f88cdf2cccf
Rounak Kumar Singh <rounak.official.2@gmail.com>
2026-03-09 14:37:37 +0530

    fix(nvim): remove cmd override to let clangd use compile_flags.txt

--- config/nvim/lua/config/lsp/servers/clangd.lua
+++ config/nvim/lua/config/lsp/servers/clangd.lua
@@ -1,13 +1,4 @@
 return {
-	cmd = {
-		"clangd",
-		"--background-index",
-		"--clang-tidy",
-		"--header-insertion=iwyu",
-		"--completion-style=detailed",
-		"--function-arg-placeholders",
-		"--fallback-style=llvm",
-	},
 	settings = {
 		clangd = {
 			inlayHints = {
